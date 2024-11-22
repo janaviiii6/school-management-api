2f09050da27aed633f072cdf3d54f2486cd79aa2
JanaviK <kharatjanavi16@gmail.com>
2024-11-22 18:58:40 +0530

    Add sequelize configuration for database connection

---
+++ db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host:  process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: true
+    }
+);
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has established successfully!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to the database: ', error);
+    });
+
+module.exports = sequelize;
