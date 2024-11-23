ea2e4ca81ec8f5b83f9028bfe3bbe10b03f63e39
JanaviK <kharatjanavi16@gmail.com>
2024-11-23 12:44:16 +0530

    Implement addSchool function and handle validations errors

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -18,4 +18,22 @@ const getListOfSchools = async (req,res) => {
     }
 }
 
-module.exports = { getListOfSchools };
+const addSchool = async (req,res) => {
+   try{
+        const { name, address, latitude, longitude } = req.body;
+
+        await School.create({ name, address, latitude, longitude });
+
+        return res.status(201).json({ message: 'School added successfully!' });
+   } catch(error) {
+        // Handling validation errors
+        if (error.name === 'SequelizeValidationError') {
+            const errorMessages = error.errors.map(e => e.message); 
+            return res.status(400).json({ errors: errorMessages });
+        }
+        return res.status(500).json({ error: 'Internal server error', error: error});
+   }
+};
+
+
+module.exports = { getListOfSchools, addSchool };
