1429fbf7133e4b43095519dbcb2a9b868a7a2f9d
JanaviK <kharatjanavi16@gmail.com>
2024-11-23 12:42:39 +0530

    Create route to add new school

--- routes/schoolRoute.js
+++ routes/schoolRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getListOfSchools } = require('../controllers/schoolController');
+const { getListOfSchools, addSchool } = require('../controllers/schoolController');
 
+//Route for fetching all school records
 router.get('/listSchools',getListOfSchools);
+
+//Route for adding school
+router.post('/addSchool',addSchool);
 
 module.exports = router;
