7565f154d5dfdff18467a3a204310db170d9e682
JanaviK <kharatjanavi16@gmail.com>
2024-11-23 14:46:30 +0530

    feat: implement school proximity school using calculateDistance utility

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,13 +1,36 @@
 const School = require('../models/school');
+const calculateDistance = require('../utils/calculateDistance');
 
 const getListOfSchools = async (req,res) => {
     try {
+
+        const { latitude, longitude } = req.query;
+        
+        if(!latitude || !longitude)
+            return res.status(400).json({ error: "Latitude and Longitude are required" });
+        
+        const userLat = parseFloat(latitude);
+        const userLon = parseFloat(longitude);
+        
+        if(isNaN(userLat) || isNaN(userLon))
+            return res.status(400).json({ error: "Invalid latitude or longitude" });
         //Fetch all the school records
         const schools = await School.findAll();
 
+        const sortedSchools = schools
+            .map((school) => {
+                const distance = calculateDistance(
+                    userLat,
+                    userLon,
+                    school.latitude,
+                    school.longitude
+                );
+                return { ...school.toJSON(), distance };
+            }).sort((a, b) => a.distance - b.distance);
+
         return res.status(200).json({
             success: true,
-            data: schools,
+            data: sortedSchools,
         });
     } catch(error) {
         console.error('Error fetching schools: ', error);
