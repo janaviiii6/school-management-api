7330c36f332b36cfa28f0aa099e5c9945da33bf7
JanaviK <kharatjanavi16@gmail.com>
2024-11-23 12:54:32 +0530

    Add validation inputs to the fields

--- models/school.js
+++ models/school.js
@@ -10,18 +10,46 @@ const School = sequelize.define('School', {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notNull: { msg: 'Please enter school name' },
+                notEmpty: { msg: 'School name cannot be empty' },
+            },
         },
         address: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notNull: { msg: 'Please enter school address' },
+                notEmpty: { msg: 'School address cannot be empty' }
+            },
         },
         latitude: {
             type: DataTypes.FLOAT,
             allowNull: false,
+            validate: {
+                notNull: { msg: 'Latitude is required' },
+                isFloat: { msg: 'Latitude must be a valid number' },
+                isValidType(value) {
+                    if(typeof value !== 'number')
+                        throw new Error('Latitude must be a number, not a string');
+                },
+                min: -90,
+                max: 90,
+            },
         },
         longitude: {
             type: DataTypes.FLOAT,
             allowNull: false,
+            validate: {
+                notNull: { msg: 'Longitude is required' },
+                isFloat: { msg: 'Longitude must be a valid number' },
+                isValidType(value) {
+                    if(typeof value !== 'number')
+                        throw new Error('Longitude must be a number, not a string');
+                },
+                min: -180,
+                max: 180,
+            },
         },
     },
     {
